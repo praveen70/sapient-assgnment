3849358e5d3725424f60400e667af57ac356a50e
praveen70 <praveen.mr288@gmail.com>
2020-09-16 16:41:46 +0530

    initisl commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sapient-assgnment
+1. yarn install
+2. yarn run build
+3. yarn start
+
---
+++ build/precache-manifest.6f6d53cd5c335293c06045407bb5d74a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4848b5a034104cf3ae30aafa0adea25f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "accbd0ee5bed92334c20",
+    "url": "/static/js/2.9c29fc0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c29fc0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13798ab1f354d7c1788f",
+    "url": "/static/js/main.9adf54f4.chunk.js"
+  },
+  {
+    "revision": "ca244d36787c68b8aa3e",
+    "url": "/static/js/runtime-main.0457053f.js"
+  }
+]);
